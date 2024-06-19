69ec81cd065336e2db59c0aeb66c1ac095c266ad
A-Chiar <amalia.chiariello@gmail.com>
2024-06-19 16:52:22 +0200

    Add files via upload

---
+++ SistemaGestioneNegozio1.py
@@ -0,0 +1,38 @@
+'''TRACCIA Lo scopo di questo esercizio è implementare un sistema di gestione per un negozio che deve interagire con clienti, 
+gestire l'inventario e permettere agli amministratori di supervisionare le operazioni. Il sistema sarà strutturato in tre parti 
+principali:
+Gestione Clienti:
+I clienti possono visualizzare gli articoli disponibili in inventario.
+I clienti possono selezionare e acquistare articoli dall'inventario.
+Il sistema tiene traccia degli acquisti dei clienti.'''
+
+controllo = True
+selezione = input("Salve, si desidera visitare il negozio? ")
+lista_prodotti = ["matita", "penna", "foglio", "agenda"]
+acquisti = []
+
+if selezione == "sì":
+    while controllo == True:
+        selezione2 = input("vuoi vedere i prodotti? ")
+        if selezione2 == "sì":
+            print(lista_prodotti)
+        else:
+            pass
+        selezione3 = input("Salve, si desidera effettuare acquisti?")
+        if selezione3 == "sì":
+            print(acquisti)
+            acquisti2 = input("cosa si intende acquistare?")
+            acquisti.append(acquisti2)
+        elif selezione3 == "no":
+            break
+        else:
+            print("risposta non valida")
+    
+
+
+        
+
+        
+
+
+            
