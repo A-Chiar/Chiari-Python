15e7de762952d37bae398dd4a292d1542f507287
A-Chiar <amalia.chiariello@gmail.com>
2024-06-20 09:49:29 +0200

    Add files via upload

--- SistemaGestioneNegozio2.py
+++ SistemaGestioneNegozio2.py
@@ -2,9 +2,10 @@
 Gli articoli in magazzino sono elencati con il nome, il prezzo e la quantità.
 È possibile aggiungere nuovi articoli all'inventario.
 Gli articoli possono essere rimossi o aggiornati (ad es., cambiare prezzo o quantità).'''
-menù = input("salve, si vuole 1. visualizzare 2. aggiungere articolo 3. modificare informazioni articoli? ")
-#creo una lista con all'interno dei dizionari, ogni dizionario corrisponde a un articolo:
-inventario =[({"nome":"matita",
+
+#è una soluzione molto poco elegante e limitata, ma almeno funziona
+'''Xcreo una lista con all'interno dei dizionari, ogni dizionario corrisponde a un articolo:X'''
+'''inventario =[({"nome":"matita",
             "prezzo": 2,
             "quantità" : 3}),
             ({"nome": "penna",
@@ -15,23 +16,28 @@ inventario =[({"nome":"matita",
             "quantità": 4}),
             ({"nome": "agenda",
             "prezzo":2,
-            "quantità": 5})]
+            "quantità": 5})]'''
+menù = input("salve, si vuole 1. visualizzare 2. aggiungere articolo 3. modificare informazioni articoli? ")
+#creare una lista
+inventario = [("matita, 2, 3"),
+            ("penna, 2, 10"),
+            ("foglio, 3, 4"),
+            ("agenda, 2,5")]
 
 if menù == "1":
+    print("Gli articoli verrano stampati con il seguente ordine: nome articolo, prezzo, quantità")
     print(inventario)
-    #almeno questo funziona
 elif menù== "2":
-    aggiunta =  input("che articoli si vogliono inserire? "),
+    aggiunta =  input("che articoli si vogliono inserire? Definire: nome articolo, prezzo, quantità"),
     #da specificare nome articolo, prezzo e quantità
-    inventario.append({aggiunta})
+    inventario.append(aggiunta)
     print(inventario)
-    #l'ultimo valore non viene aggiunto come chiave-valore, ma solo come una serie di valori
 elif menù == "3":
-    #non funziona
     print(inventario)
-    modificaposizione = input("che posizione si desidera cambiare?")
+    modificaposizione = int(input("che posizione si desidera cambiare?")) #è necessario che sia un intero, essendo una posizione
     modificavalore = input("che valore si desidera cambiare?")
-    inventario[modificaposizione] = {modificavalore}
+    inventario[modificaposizione] = modificavalore
+    print(inventario)
 else:
     print("uscita da inventario")
 
