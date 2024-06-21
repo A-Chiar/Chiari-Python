04fd940cc705e6ff40909e8f2d9584e48ef84fa7
A-Chiar <amalia.chiariello@gmail.com>
2024-06-21 14:39:46 +0200

    Add files via upload

---
+++ esercizio3.py
@@ -0,0 +1,8 @@
+#inizializzo le due variabili di input
+massimo = int(input("inserire un numero massimo "))
+
+step = int(input("inserire uno step "))
+
+#creo il ciclo for per stampare i valori
+for i in range(0,massimo,step):
+    print(i)
