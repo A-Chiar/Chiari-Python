ef9d41f54363a1b0f449fec018ad232c512af669
A-Chiar <amalia.chiariello@gmail.com>
2024-06-21 14:22:53 +0200

    Add files via upload

--- esercizio1.py
+++ esercizio1.py
@@ -1,11 +1,13 @@
+#inizializzo le variabili
 numero = int(input("inserire un numero intero "))
-lista_numeri = []
+lista_numeri = [] #la lista vuota mi servirà per la successiva somma
 conteggio = 0
 
-while numero != 0:
+while numero != 0: #se il numero è diverso da zero, aggiunge il numero alla lista e chiede di inserire un altro numero
     lista_numeri.append(numero)
     numero = int(input("inserisci un altro numero "))
     if numero == 0:
         for i in lista_numeri:
             conteggio += i
-        print("la somma dei numeri inseriti è: ", conteggio)
+        print("la somma dei numeri inseriti è: ", conteggio) #stampa la somma
+        
