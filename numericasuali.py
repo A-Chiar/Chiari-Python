1374436a0701d199c5c0693cad01264622794661
A-Chiar <amalia.chiariello@gmail.com>
2024-06-26 14:31:16 +0200

    Add files via upload

--- numericasuali.py
+++ numericasuali.py
@@ -8,24 +8,34 @@ import random
 random_numbers = []
 num_input = 5
 
-
-
-
 #Usa un ciclo for per generare i numeri casuali e aggiungerli alla lista
-for i in range(num_input):
-    random_number = random.randint(1, 6)  # Genera un numero intero casuale tra 1 e 100
-    random_numbers.append(random_number)
+while num_input > 0:
+    random_number = random.randint(1, 6) 
+    if str(random_number) not in random_numbers:
+        random_numbers.append(str(random_number))
+        num_input -= 1
     
-#separatore = " "
+                                                                        # Genera un numero intero casuale tra 1 e 100
+    
 with open("numericasuali.txt","w") as file:
-    file.write("".join(map(str,random_numbers)))
+    file.write(",".join(random_numbers))
 
-'''with open("numericasuali.txt","r") as file:
+with open("numericasuali.txt","r") as file:
     contenuto = file.read()
-print(type(contenuto))'''
+print(contenuto)
 
-input = input("inserisci 5 valori ")
-
-for i in range(num_input):
-    if input in file: 
-        print("ok")
+lista = contenuto.split(",")
+print(lista)
+contatore= 5
+indovinate = 0
+while contatore > 1:
+    valore = input("inserisci 5 valori ")
+    if valore in lista:
+        indovinate += 1
+        print("hai indovinato un numero")
+    contatore -= 1
+if indovinate >1:
+    print("hai vinto")
+else:
+    print("hai perso")
+    
