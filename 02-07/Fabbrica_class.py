b93c46dfa0c202f0e966702f57b7ef7bcfdb44f1
A-Chiar <amalia.chiariello@gmail.com>
2024-07-02 11:37:29 +0200

    Add files via upload

---
+++ 02-07/Fabbrica_class.py
@@ -0,0 +1,50 @@
+class Prodotto():
+
+    def __init__(self, nome, costo_produzione, prezzo_vendita):
+        self.nome = nome
+        self.costo_produzione = costo_produzione
+        self.prezzo_vendita = prezzo_vendita
+    
+    def calcola_profitto(self):
+        return self.prezzo_vendita-self.costo_produzione
+
+    class Elettronica(): ##non so come fare 
+        def __init__(self, garanzia):
+            self.garanzia = garanzia
+    
+    class Abbigliamento():
+        def __init__(self, materiale):
+            self.materiale = materiale
+        
+
+class Fabbrica():
+    inventario = {"forbice":1,"gatto":4}
+    
+
+    def aggiungi_prodotto(self):
+        domanda = input("vuoi aggiungere un prodotto? ")
+        while domanda.lower() == "sì":
+            aggiunta = input("che prodotto vuoi aggiungere ")
+            quantità = input("quanti prodotti vuoi aggiungere ")
+            self.inventario[aggiunta] = quantità
+            return self.inventario
+    
+    def vendi_prodotto(self):
+        vendita = input("inserire il prodotto venduto ")
+        quantità = int(input("inserire la quantità venduta "))
+        if vendita in self.inventario:
+            self.inventario[vendita] -= quantità #fare in modo di ottenere una sottrazione tar il prodotto presente e la quantità venduta
+            return self.inventario
+
+    def resi_prodotto(self):
+        vendita = input("inserire il nome del prodotto reso ")
+        quantità = int(input("inserire la quantità di prodotto resa "))
+        if vendita in self.inventario:
+            self.inventario[vendita] += quantità
+            return self.inventario
+
+
+        
+prodotto = Prodotto("prodotto",3,4)
+fabbrica = Fabbrica()
+print(fabbrica.resi_prodotto())
