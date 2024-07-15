bb0e729bb9f61d94713302aa94a870c58de0eada
A-Chiar <amalia.chiariello@gmail.com>
2024-07-15 17:40:30 +0200

    Add files via upload

---
+++ 15-07/esercizio2slicingnum.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr = np.random.randint(10,50, size = 20)
+print(arr)
+
+primi_dieci = arr[0:11]
+print(primi_dieci)
+
+ultimi_cinque = arr[-5:-1] #da rivedere
+print(ultimi_cinque)
+
+daindice5_a15 = arr[5:15]
+print(daindice5_a15)
+
+#terzo_elemento = arr[:,2:3] #dà errore in quanto l'array è 1-dimensionale
+
+#print(terzo_elemento)
+
+arr[5:10] = 99
+print(arr)
